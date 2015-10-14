663f8d85505a411c8538d50f46b0cd165d9593d8
sjorskamphuis <sjorskamphuis99@gmail.com>
2015-10-14 10:31:40 +0200

    add new folder client-size

---
+++ code/vincent_sjors_quiz_master/client-side/assets/js/quizzer.js
@@ -0,0 +1,18 @@
+var quizzer = angular.module("quizzerApp", ['ngRoute']);
+
+quizzer.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/partials/team.html'
+    })
+
+    .when('/master', {
+      templateUrl: '/partials/master.html'
+    })
+})
+
+quizzer.controller("quizzerController", function($scope, $routeParams) {
+  $scope.currentRoute = $routeParams;
+
+  console.log($scope.currentRoute);
+});
