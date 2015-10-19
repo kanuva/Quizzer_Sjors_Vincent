416bd3f185e0a6ad8f411694cd93a2e6811873ef
sjorskamphuis <sjorskamphuis99@gmail.com>
2015-10-19 21:12:24 +0200

    added websockets

--- code/vincent_sjors_quiz_master/app.js
+++ code/vincent_sjors_quiz_master/app.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var quizzer = express();
 var path = require('path');
 
+var express = require('express');
+var quizzer = express();
 
+var WebSocket = require('ws').Server;
+var ws = new WebSocket({port: 8080});
+
+//Express
 quizzer.use(express.static(path.join(__dirname, 'client-side')));
 
 quizzer.get('/', function (req, res) {
@@ -10,4 +14,16 @@ quizzer.get('/', function (req, res) {
 });
 
 
-var server = quizzer.listen(3000);
+//WebSocket
+ws.on('open', function connection() {
+  ws.send('test');
+});
+
+ws.on('message', function incomming(message) {
+  console.log('message recieved');
+});
+
+
+var server = quizzer.listen(3000, function() {
+  console.log('EXPRESS SERVER IS RUNNING');
+});
